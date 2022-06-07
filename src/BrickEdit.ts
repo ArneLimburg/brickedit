b5c0943e4629150d12eb11732f46af2c4318007d
arne <arne@apache.org>
2022-06-07 23:10:00 +0200

    Add rendering of LDraw Model

--- src/BrickEdit.ts
+++ src/BrickEdit.ts
@@ -1,86 +1,132 @@
-/**
- * @license
- * Copyright 2022 Arne Limburg
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+/* eslint-disable no-var */
+/* eslint-disable vars-on-top */
+import {
+  AmbientLight,
+  Box3,
+  BufferGeometry,
+  Camera,
+  Group,
+  Material,
+  Mesh,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  Vector3,
+  WebGLRenderer,
+} from 'three';
+import { LDrawLoader } from './loaders/LDrawLoader.js';
+// import { OrbitControls } from './controls/OrbitControls.js';
 
-import { LitElement, html, css } from 'lit';
-import { property } from 'lit/decorators.js';
+export class BrickEdit extends HTMLElement {
+  host: HTMLElement | undefined;
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+  canvas: any;
 
-export class BrickEdit extends LitElement {
-  @property({ type: String }) title = 'My app';
+  renderer: any;
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--brick-edit-background-color);
-    }
+  scene: any;
 
-    main {
-      flex-grow: 1;
-    }
+  camera: Camera | undefined;
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
+  geometry: BufferGeometry | undefined;
 
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
+  material: Material | undefined;
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
+  cube: Mesh<BufferGeometry, Material> | undefined;
 
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+  constructor() {
+    super();
 
-  render() {
-    return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
+    const template = document.createElement('template');
+    template.innerHTML = `
+<style>
+:host {
+	display: block;
+	margin: 0;
+	height: 100%;
+}
 
-        <p>Edit <code>src/BrickEdit.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+#c {
+	width: 100%;
+	height: 100%;
+	display: block;
+	border:0px solid red;
+}
+</style>
+<canvas id="c"></canvas>`;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot?.appendChild(template.content.cloneNode(true));
+    this.host = this.shadowRoot?.host as HTMLElement; // use of host might be unnecessary
+    this.canvas = this.shadowRoot?.querySelector('#c');
+  }
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
-    `;
+  connectedCallback() {
+    // fires with constructor is done.
+    this.init();
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  init() {
+    this.camera = new PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000
+    );
+    this.camera.position.set(0, 0, 500);
+
+    // scene
+
+    this.scene = new Scene();
+
+    const ambientLight = new AmbientLight(0xcccccc, 0.4);
+    this.scene.add(ambientLight);
+
+    const pointLight = new PointLight(0xffffff, 1);
+    pointLight.position.set(-1000, 1200, 1500);
+    this.scene.add(pointLight);
+
+    //
+
+    this.renderer = new WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //    const controls = new OrbitControls( this.camera, this.renderer.domElement );
+    const ldrawPath = 'models/ldraw/officialLibrary/';
+
+    var model: Group;
+    const lDrawLoader = new LDrawLoader();
+    const { camera, canvas, renderer, scene } = this;
+    lDrawLoader.setPath(ldrawPath).load(
+      'models/car.ldr_Packed.mpd',
+      (group: Group) => {
+        if (model) {
+          scene.remove(model);
+        }
+
+        model = group;
+
+        // Convert from LDraw coordinates: rotate 180 degrees around OX
+        model.rotation.x = Math.PI;
+
+        scene.add(model);
+
+        // Adjust camera and light
+
+        var bbox = new Box3().setFromObject(model);
+        var size = bbox.getSize(new Vector3());
+        var radius = Math.max(size.x, Math.max(size.y, size.z)) * 0.5;
+
+        pointLight.position.normalize().multiplyScalar(radius * 3);
+        renderer.render(scene, camera);
+        renderer.gammaInput = true;
+        renderer.gammaOutput = true;
+        const { width, height } = canvas;
+        renderer.setSize(width, height, false);
+      },
+      () => {},
+      () => {}
+    );
   }
 }
--- test/brick-edit.test.ts
+++ test/brick-edit.test.ts
@@ -10,12 +10,6 @@ describe('BrickEdit', () => {
     element = await fixture(html`<brick-edit></brick-edit>`);
   });
 
-  it('renders a h1', () => {
-    const h1 = element.shadowRoot!.querySelector('h1')!;
-    expect(h1).to.exist;
-    expect(h1.textContent).to.equal('My app');
-  });
-
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
