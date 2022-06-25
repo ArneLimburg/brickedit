eb7ec26f1754ceec8b8505c50a45e51a16be0627
arne <arne@apache.org>
2022-06-25 09:42:26 +0200

    Introduce ModelLoader

---
+++ src/loaders/ModelLoader.ts
@@ -0,0 +1,113 @@
+import { FileLoader, Group } from 'three';
+import { LDrawLoader } from './LDrawLoader.js';
+import { ModelInfo } from './ModelInfo.js';
+import { Submodel } from './Submodel.js';
+import { ModelLine } from '../model/ModelLine.js';
+import { Model } from '../model/Model.js';
+
+export class ModelLoader {
+  readonly lDrawLoader;
+
+  constructor(loader: LDrawLoader) {
+    this.lDrawLoader = loader;
+  }
+
+  async load(fileName: string, content: string): Promise<Model> {
+    const modelGroup = new Group();
+    // Convert from LDraw coordinates: rotate 180 degrees around OX
+    modelGroup.rotation.x = Math.PI;
+    await this.fillCache(this.lDrawLoader.partsLibraryPath + fileName);
+    const info = this.lDrawLoader.partsCache.parseCache.parse(
+      content
+    ) as ModelInfo;
+
+    const modelLines = await this.buildLines(info, modelGroup);
+    return new Model(fileName, content, modelGroup, modelLines);
+  }
+
+  private async fillCache(url: string) {
+    let loadingSuccessful: () => void;
+    let loadingFailed: (reason: any) => void;
+    const loading = new Promise<void>((resolve, reject) => {
+      loadingSuccessful = resolve;
+      loadingFailed = reject;
+    });
+    this.lDrawLoader.load(
+      url,
+      () => loadingSuccessful(),
+      () => {},
+      (reason?: any) => loadingFailed(reason)
+    );
+    return loading;
+  }
+
+  private async buildLines(
+    info: ModelInfo,
+    modelGroup: Group
+  ): Promise<ModelLine[]> {
+    if (!info.subobjects) {
+      return Promise.resolve([]);
+    }
+    const lines = info.subobjects.map(submodel => this.buildLine(submodel));
+    return Promise.all(lines).then(loadedLines => {
+      loadedLines.forEach(line => modelGroup.add(line.group));
+      return loadedLines;
+    });
+  }
+
+  private async buildLine(model: Submodel) {
+    let loaded: (line: ModelLine) => void;
+    let failed: () => void;
+    const loadedLine = new Promise<ModelLine>(
+      (resolve: (line: ModelLine) => void, reject: () => void) => {
+        loaded = resolve;
+        failed = reject;
+      }
+    );
+    this.loadPart(
+      `models/ldraw/parts/${model.fileName}`,
+      model.colorCode,
+      (group: Group) => {
+        group.applyMatrix4(model.matrix);
+
+        loaded(new ModelLine(model.line, model.lineIndex, group));
+      },
+      () => {},
+      () => failed()
+    );
+    return loadedLine;
+  }
+
+  private loadPart(
+    url: string,
+    colorCode: string,
+    onLoad: (group: Group) => void,
+    onProgress: () => void,
+    onError: () => void
+  ) {
+    const fileLoader = new FileLoader(this.lDrawLoader.manager);
+    fileLoader.setPath(this.lDrawLoader.path);
+    fileLoader.setRequestHeader(this.lDrawLoader.requestHeader);
+    fileLoader.setWithCredentials(this.lDrawLoader.withCredentials);
+    fileLoader.load(
+      url,
+      text => {
+        this.lDrawLoader.partsCache
+          .parseModel(text)
+          .then(group => {
+            this.lDrawLoader.applyMaterialsToMesh(
+              group,
+              colorCode,
+              this.lDrawLoader.materialLibrary,
+              true
+            );
+            this.lDrawLoader.computeConstructionSteps(group);
+            onLoad(group);
+          })
+          .catch(onError);
+      },
+      onProgress,
+      onError
+    );
+  }
+}
--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,7 +1,4 @@
-import { FileLoader, Group } from 'three';
-import { LDrawLoader } from '../loaders/LDrawLoader.js';
-import { ModelInfo } from '../loaders/ModelInfo.js';
-import { Submodel } from '../loaders/Submodel.js';
+import { Group } from 'three';
 import { ModelLine } from './ModelLine.js';
 
 export class Model {
@@ -9,100 +6,23 @@ export class Model {
 
   readonly lines: string[];
 
-  readonly loader;
-
   readonly modelGroup;
 
-  readonly modelLines: Promise<ModelLine[]>;
+  readonly modelLines: ModelLine[];
 
   constructor(
     fileName: string,
     content: string,
-    info: ModelInfo,
-    loader: LDrawLoader
+    modelGroup: Group,
+    modelLines: ModelLine[]
   ) {
     this.fileName = fileName;
     this.lines = content.split('\n');
-    this.loader = loader;
-    this.modelGroup = new Group();
-    // Convert from LDraw coordinates: rotate 180 degrees around OX
-    this.modelGroup.rotation.x = Math.PI;
-    this.modelLines = this.buildLines(info);
+    this.modelGroup = modelGroup;
+    this.modelLines = modelLines;
   }
 
-  private buildLines(info: ModelInfo): Promise<ModelLine[]> {
-    if (!info.subobjects) {
-      return Promise.resolve([]);
-    }
-    const lines = info.subobjects.map(submodel => this.buildLine(submodel));
-    return Promise.all(lines).then(loadedLines => {
-      loadedLines.forEach(line => this.modelGroup.add(line.group));
-      return loadedLines;
-    });
-  }
-
-  async loadLines() {
-    return this.modelLines;
-  }
-
-  async findLine(group: Group) {
-    const modelLines = await this.loadLines();
-    return modelLines.find(line => line.group === group);
-  }
-
-  async buildLine(model: Submodel) {
-    let loaded: (line: ModelLine) => void;
-    let failed: () => void;
-    const loadedLine = new Promise<ModelLine>(
-      (resolve: (line: ModelLine) => void, reject: () => void) => {
-        loaded = resolve;
-        failed = reject;
-      }
-    );
-    this.loadPart(
-      `models/ldraw/parts/${model.fileName}`,
-      model.colorCode,
-      (group: Group) => {
-        group.applyMatrix4(model.matrix);
-
-        loaded(new ModelLine(model.line, model.lineIndex, group));
-      },
-      () => {},
-      () => failed()
-    );
-    return loadedLine;
-  }
-
-  loadPart(
-    url: string,
-    colorCode: string,
-    onLoad: (group: Group) => void,
-    onProgress: () => void,
-    onError: () => void
-  ) {
-    const fileLoader = new FileLoader(this.loader.manager);
-    fileLoader.setPath(this.loader.path);
-    fileLoader.setRequestHeader(this.loader.requestHeader);
-    fileLoader.setWithCredentials(this.loader.withCredentials);
-    fileLoader.load(
-      url,
-      text => {
-        this.loader.partsCache
-          .parseModel(text)
-          .then(group => {
-            this.loader.applyMaterialsToMesh(
-              group,
-              colorCode,
-              this.loader.materialLibrary,
-              true
-            );
-            this.loader.computeConstructionSteps(group);
-            onLoad(group);
-          })
-          .catch(onError);
-      },
-      onProgress,
-      onError
-    );
+  findLine(group: Group) {
+    return this.modelLines.find(line => line.group === group);
   }
 }
