ef6eaba32645d819538a98494f829c0ae277130f
arne <arne@apache.org>
2022-06-29 22:10:50 +0200

    Implement dragging of parts

--- src/selection/PartSelector.ts
+++ src/selection/PartSelector.ts
@@ -1,4 +1,13 @@
-import { Color, Group, Material, Mesh, Raycaster, Vector2 } from 'three';
+import {
+  Color,
+  Group,
+  Material,
+  Mesh,
+  Plane,
+  Raycaster,
+  Vector2,
+  Vector3,
+} from 'three';
 import { ModelLine } from '../model/ModelLine.js';
 import { ModelPane } from '../ModelPane.js';
 
@@ -9,6 +18,10 @@ export class PartSelector {
 
   selection?: Mesh;
 
+  distanceOfSelectionFromCamera?: number;
+
+  movementStart?: Vector3;
+
   originalMaterialOfSelection?: Material | Material[];
 
   originalColorOfSelection?: Color;
@@ -16,12 +29,17 @@ export class PartSelector {
   readonly selectionListener = (event: MouseEvent) =>
     this.updateSelection(event);
 
+  readonly moveListener = (event: MouseEvent) => this.moveSelection(event);
+
+  readonly moveFinishedListener = (event: MouseEvent) =>
+    this.selectionMoved(event);
+
   constructor(modelPane: ModelPane) {
     this.modelPane = modelPane;
   }
 
   register(canvas: HTMLCanvasElement) {
-    canvas.addEventListener('click', this.selectionListener);
+    canvas.addEventListener('mousedown', this.selectionListener);
   }
 
   reregister(canvas: HTMLCanvasElement) {
@@ -30,10 +48,16 @@ export class PartSelector {
   }
 
   unregister(canvas: HTMLCanvasElement) {
-    canvas.removeEventListener('click', this.selectionListener);
+    canvas.removeEventListener('mousedown', this.selectionListener);
+    canvas.removeEventListener('mousemove', this.moveListener);
   }
 
   updateSelection(mouseEvent: MouseEvent) {
+    mouseEvent.target?.addEventListener('mousemove', this.moveListener as any);
+    mouseEvent.target?.addEventListener(
+      'mouseup',
+      this.moveFinishedListener as any
+    );
     const clickLocation = new Vector2();
     clickLocation.x = (mouseEvent.clientX / window.innerWidth) * 2 - 1;
     clickLocation.y = -(mouseEvent.clientY / window.innerHeight) * 2 + 1;
@@ -53,6 +77,10 @@ export class PartSelector {
         this.selection.material = this.originalMaterialOfSelection;
       }
       this.selection = selectedObject.object as Mesh;
+      this.distanceOfSelectionFromCamera = selectedObject.distance;
+      this.movementStart = new Vector3();
+      this.selection?.getWorldPosition(this.movementStart);
+
       this.originalMaterialOfSelection = this.selection.material;
       if ((this.selection.material as Material).isMaterial) {
         const newMaterial = (
@@ -79,6 +107,33 @@ export class PartSelector {
       this.selected(parent);
     }
     this.modelPane.render();
+  }
+
+  moveSelection(mouseEvent: MouseEvent) {
+    if (this.distanceOfSelectionFromCamera && this.movementStart) {
+      const plane = new Plane(new Vector3(0, 1, 0), 0);
+      const clickLocation = new Vector2();
+      clickLocation.x = (mouseEvent.clientX / window.innerWidth) * 2 - 1;
+      clickLocation.y = -(mouseEvent.clientY / window.innerHeight) * 2 + 1;
+      const raycaster = new Raycaster();
+      raycaster.setFromCamera(clickLocation, this.modelPane.camera);
+      const intersection = new Vector3();
+      raycaster.ray.intersectPlane(plane, intersection);
+      this.selection?.position.set(
+        intersection.x,
+        intersection.y,
+        intersection.z
+      );
+
+      this.modelPane.render();
+    }
+  }
+
+  selectionMoved(mouseEvent: MouseEvent) {
+    mouseEvent.target?.removeEventListener(
+      'mousemove',
+      this.moveListener as any
+    );
   }
 
   async selected(group?: Group) {
